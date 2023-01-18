0601361607a59210103425c2a701462c665b6cec
Art <hirche.arthur@gmail.com>
2023-01-18 18:05:01 -0300

    Exercícios do Curso em Vídeo

--- pythonExercicios/ex019.py
+++ pythonExercicios/ex019.py
@@ -1,7 +1,8 @@
-import random
-nome1 = str('Pedro')
-nome2 = str('Henrico')
-nome3 = str('Arthur')
-nome4 = str('Gustavo')
-sorteio = random.random(nome1, nome2, nome3, nome4)
-print(f'Entre os alunos {nome1}, {nome2}, {nome3} e {nome4}, irá limpar o quadro o {sorteio}.')
+from random import choice
+nome1 = str(input('Nome do primeiro aluno: '))
+nome2 = str(input('Nome do segundo aluno: '))
+nome3 = str(input('Nome do terceiro aluno: '))
+nome4 = str(input('Nome do quarto aluno: '))
+lista = [nome1, nome2, nome3, nome4]
+sorteado = choice(lista)
+print(f'Entre os alunos {nome1}, {nome2}, {nome3} e {nome4}, irá limpar o quadro o {sorteado}.')
