5979a325e7f5aeba20c5fb9c0d0931efc8cf3916
Art <hirche.arthur@gmail.com>
2023-02-09 11:40:52 -0300

    Exercícios do Curso em Vídeo

--- pythonExercicios/ex036.py
+++ pythonExercicios/ex036.py
@@ -1,12 +1,14 @@
 valorCasa = float(input('Qual é o valor da casa? '))
-salário = float(input('Quanto você ganha? R$ '))
+salario = float(input('Quanto você ganha? R$ '))
 pagarAnos = int(input('Em quantos anos você irá pagar? '))
 prestação = valorCasa/(pagarAnos*12)
+minimo = salario * (30/100)
 print('-'*30)
 print(f'Você irá pagar R$ {prestação:.2f} por mês.')
-if prestação > (salário+(30/100)):
+if prestação > minimo:
     print('\033[1;31mInfelizmente você não conseguirá financiar essa casa.\033[m')
 else:
     print('\033[1;32mVocê conseguirá financiar essa casa!\033[m')
 print('\033[4mFim da simulação.\033[m')
 print('-'*30)
+#Correto!
